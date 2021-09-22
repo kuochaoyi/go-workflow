5ca56dd5394b1ccb93cf0d2c2061527baa33cba9
KuoChaoYi <master@wisdomfish.org>
2021-09-22 11:24:34 +0800

    Refactoring dbms.

--- internal/pkg/dao/database.go
+++ internal/pkg/dao/database.go
@@ -1,51 +1,56 @@
 package dao
 
 import (
+	"log"
 	"strconv"
 
+	config "github.com/kuochaoyi/go-workflow/workflow-config"
 	"github.com/kuochaoyi/go-workflow/workflow-engine/model"
 
 	"github.com/jinzhu/gorm"
 )
 
-//
-//var db *gorm.DB
-//
-//// Model 其它数据结构的公共部分
-//type Model struct {
-//	ID int `gorm:"primary_key" json:"id,omitempty"`
-//}
-//
-//// 配置
-//var conf = *config.Config
+var db *gorm.DB
+
+// Model 其它数据结构的公共部分
+type Model struct {
+	ID int `gorm:"primary_key" json:"id,omitempty"`
+}
+
+// 配置
+var conf = *config.Config
 
 // Setup 初始化一个db连接
 func Setup() {
 	//var err error
-	//log.Println("数据库初始化！！")
+	log.Println("数据库初始化！！")
 	//db, err = gorm.Open(conf.DbType, fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", conf.DbUser, conf.DbPassword, conf.DbHost, conf.DbPort, conf.DbName))
 	//if err != nil {
 	//	log.Fatalf("数据库连接失败 err: %v", err)
 	//}
 	// 启用Logger，显示详细日志
-	mode, _ := strconv.ParseBool(conf.DbLogMode)
+	db, _ := New()
 
-	db.LogMode(mode)
+	//mode, _ := strconv.ParseBool(conf.DbLogMode)
+	//
+	//db.LogMode(mode)
 
-	db.SingularTable(true) //全局设置表名不可以为复数形式
+	//db.SingularTable(true) //全局设置表名不可以为复数形式
+
 	// db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
-	idle, err := strconv.Atoi(conf.DbMaxIdleConns)
-	if err != nil {
-		panic(err)
-	}
-	db.DB().SetMaxIdleConns(idle)
-	open, err := strconv.Atoi(conf.DbMaxOpenConns)
-	if err != nil {
-		panic(err)
-	}
-	db.DB().SetMaxOpenConns(open)
+	//idle, err := strconv.Atoi(conf.DbMaxIdleConns)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//db.DB().SetMaxIdleConns(idle)
+	//open, err := strconv.Atoi(conf.DbMaxOpenConns)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//db.DB().SetMaxOpenConns(open)
 
-	db.Set("gorm:table_options", "ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;").AutoMigrate(&model.Procdef{}).
+	//db.Set("gorm:table_options", "ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;").AutoMigrate(&model.Procdef{}).
+	db.AutoMigrate(&model.Procdef{}).
 		AutoMigrate(&model.Execution{}).AutoMigrate(&model.Task{}).
 		AutoMigrate(&model.ProcInst{}).AutoMigrate(&model.Identitylink{}).
 		AutoMigrate(&model.ExecutionHistory{}).
